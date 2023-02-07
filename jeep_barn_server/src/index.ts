e1d661c4e7d1d01f16d7b0a40de931155734f50a
ewu32 <eric.wu151@gmail.com>
2023-02-06 21:49:59 -0700

    JEEP-12 Added test calendar and server response

--- jeep_barn_server/src/index.ts
+++ jeep_barn_server/src/index.ts
@@ -3,41 +3,22 @@ import { PrismaClient } from "@prisma/client";
 const client = new PrismaClient();
 const app = express();
 
+var cors = require("cors");
+
+app.use(cors());
 app.use(express.json());
 
-type CreateUserBody = {
-  firstName: string,
-  lastName: string,
-  email: string,
-  password: string,
+type ReserveDateBody = {
+  date: Date,
+  jeep: any
 }
 
-app.post('/users', async (req, res) => {
-  const {firstName, lastName, email, password} = req.body as CreateUserBody;
-  const user = await client.user.create({ data: {
-    firstName,
-    lastName,
-    email,
-    password
-  }});
-  res.json({ user });
-});
-
-app.get("/users", async (req, res) => {
-  const users = await client.user.findMany({
-    include: {
-      sessions: {
-        include: {
-          user: true
-        }
-      }
-    }
+app.put("/", (req, res) => {
+  const {date, jeep} = req.body as ReserveDateBody;
+  res.json({
+    date,
+    jeep,
   });
-  res.json({ users });
-})
-
-app.get("/", (req, res) => {
-  res.send(`<h1>Hello, world!</h1>`);
 });
 
 app.listen(3000, () => {
